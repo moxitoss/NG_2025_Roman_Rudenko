41531912c100bd3750add2561b3658edbafbe2cb
Roman <215273536+moxitoss@users.noreply.github.com>
2025-11-08 19:12:43 +0200

    Замінив старий main.cpp

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main()
 {
     int stars;
+    cout << "Hello! This program will draw a triangle made of stars. You can choose how many rows the triangle will have. Good luck!" << endl;
     cout << "Write the number of stars: ";
     cin >> stars;
     int row = 1;
     while(row <= stars){
         int column = 1;
         while(column <= row){
-           cout << "*";
-           ++column;
+            cout << "*";
+            ++column;
         }
         cout << endl;
         ++row;
     }
     return 0;
 }
+
